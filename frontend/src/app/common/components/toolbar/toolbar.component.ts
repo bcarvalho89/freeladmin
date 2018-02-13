4b06cf9a3f5acc85f34ba8ca548624734526132e
Bruno Carvalho <bruno@brunocarvalho.me>
2018-02-13 18:48:41 -0200

    Add support for fullscreen

--- frontend/src/app/common/components/toolbar/toolbar.component.ts
+++ frontend/src/app/common/components/toolbar/toolbar.component.ts
@@ -11,6 +11,8 @@ import { AuthenticationService } from '../../services/authentication.service';
 export class ToolbarComponent implements OnInit {
   @Input() leftnav;
   private showMenuTrigger = false;
+  public isFullscreen = false;
+  public fullScreenAvailable = document.fullscreenEnabled || document.webkitFullscreenEnabled;
 
   constructor(
     private authService: AuthenticationService,
@@ -22,12 +24,39 @@ export class ToolbarComponent implements OnInit {
     this.leftnav.onClose.subscribe(() => this.showMenuTrigger = true);
   }
 
+  private _requestFullScreen(element) {
+    if (element.requestFullscreen) {
+      element.requestFullscreen();
+    } else if (element.webkitRequestFullscreen) {
+      element.webkitRequestFullscreen();
+    }
+  }
+
+  private _exitFullScreen() {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+  }
+
   changeLanguage(code) {
     this.translate.use(code);
   }
 
   doLogout() {
     this.authService.logout();
+  }
+
+  toggleFullscreen() {
+
+    if (this.isFullscreen) {
+      this._exitFullScreen();
+    } else {
+      this._requestFullScreen(document.documentElement);
+    }
+
+    this.isFullscreen = !this.isFullscreen;
   }
 
 }
