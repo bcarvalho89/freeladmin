cad5efc6ca7c1d839aac0feb07fd066dc350994d
Bruno Carvalho <bruno@brunocarvalho.me>
2018-02-14 15:02:58 -0200

    Add submenu

--- frontend/src/app/main/main.component.ts
+++ frontend/src/app/main/main.component.ts
@@ -7,9 +7,63 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  public menuList: any;
+  public selected: any;
+
+  constructor() {
+    this.menuList = [
+      {
+        label: 'MAIN_MENU.HOME',
+        link: [''],
+        icon: 'home'
+      },
+      {
+        label: 'MAIN_MENU.CONTACTS',
+        link: ['contacts'],
+        icon: 'account_box'
+      },
+      {
+        label: 'MAIN_MENU.TODO',
+        link: ['todo'],
+        icon: 'check_box'
+      },
+      {
+        label: 'MAIN_MENU.CALENDAR',
+        link: ['calendar'],
+        icon: 'today'
+      },
+      {
+        label: 'MAIN_MENU.PROJECTS',
+        link: ['projects'],
+        icon: 'apps'
+      },
+      {
+        label: 'MAIN_MENU.INVOICES',
+        link: ['invoices'],
+        icon: 'receipt'
+      },
+      {
+        label: 'MAIN_MENU.SETTINGS',
+        icon: 'settings',
+        submenu: [
+          {
+            label: 'MAIN_MENU.USERS',
+            link: ['']
+          }
+        ]
+      }
+    ];
+  }
 
   ngOnInit() {
+  }
+
+  selectSubmenuItem(item) {
+    this.selected = (this.selected === item ? null : item);
+  }
+
+  isActiveSubmenu(item) {
+    return this.selected === item;
   }
 
 }
